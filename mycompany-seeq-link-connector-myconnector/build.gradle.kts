50f5ebca15fe6dbef5e84bbed9263a593bad0a3d
Marius Oancea <marius.oancea@seeq.com>
2022-04-04 21:20:38 +0300

    Prepare 54.3.0 tag

--- mycompany-seeq-link-connector-myconnector/build.gradle.kts
+++ mycompany-seeq-link-connector-myconnector/build.gradle.kts
@@ -7,11 +7,9 @@ group = "com.mycompany.seeq.link.connector"
 version = "0.1.0-SNAPSHOT"
 
 dependencies {
-    compileOnly("com.seeq.link:seeq-link-sdk:54.3.0-v202201281139")
-    //    compileOnly("com.seeq.link:seeq-link-sdk:55.1.0-v202202032202-BETA")
+    compileOnly("com.seeq.link:seeq-link-sdk:54.3.0-v202203032114")
 
-    testImplementation("com.seeq.link:seeq-link-sdk:54.3.0-v202201281139")
-    //    testImplementation("com.seeq.link:seeq-link-sdk:55.1.0-v202202032202-BETA")
+    testImplementation("com.seeq.link:seeq-link-sdk:54.3.0-v202203032114")
     testImplementation("junit:junit:4.12")
     testImplementation("org.mockito:mockito-core:4.1.0")
     testImplementation("org.assertj:assertj-core:3.19.0")
--- seeq-link-sdk-debugging-agent/build.gradle.kts
+++ seeq-link-sdk-debugging-agent/build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.seeq.link:seeq-link-agent:54.3.0-v202201281139")
-    //    implementation("com.seeq.link:seeq-link-agent:55.1.0-v202202032202-BETA")
+    implementation("com.seeq.link:seeq-link-agent:54.3.0-v202203032114")
 }
 
 application {
