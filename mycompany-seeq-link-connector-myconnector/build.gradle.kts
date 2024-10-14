76fb498b8d601265d89ea53e938cea4e295f8e4d
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-10-14 20:58:32 +0100

    CRAB-44041: use published packages instead of snapshots

--- mycompany-seeq-link-connector-myconnector/build.gradle.kts
+++ mycompany-seeq-link-connector-myconnector/build.gradle.kts
@@ -7,13 +7,13 @@ group = "com.mycompany.seeq.link.connector"
 version = "0.1.0-SNAPSHOT"
 
 dependencies {
-    compileOnly("com.seeq.link:seeq-link-sdk:66.0.0-v202409241536-SNAPSHOT")
+    compileOnly("com.seeq.link:seeq-link-sdk:66.0.0-v202410141803")
 
-    testImplementation("com.seeq.link:seeq-link-sdk:66.0.0-v202409241536-SNAPSHOT")
+    testImplementation("com.seeq.link:seeq-link-sdk:66.0.0-v202410141803")
     testImplementation("org.mockito:mockito-core:4.1.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation(testFixtures("com.seeq.link:seeq-link-sdk:66.0.0-v202409241536-SNAPSHOT"))
+    testImplementation(testFixtures("com.seeq.link:seeq-link-sdk:66.0.0-v202410141803"))
 }
 
 tasks {
